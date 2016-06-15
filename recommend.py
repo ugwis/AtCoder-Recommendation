98b9669efba1a54fb86cd302f8806a9dc99d4714
Kitamura Yuta <ugwis@icloud.com>
2016-06-16 08:43:08 +0900

    問題を推薦

--- recommend.py
+++ recommend.py
@@ -17,6 +17,23 @@ import math
 url_media = "https://upload.twitter.com/1.1/media/upload.json"
 url_text = "https://api.twitter.com/1.1/statuses/update.json"
 
+def fetch_count_problems():
+    connector = psycopg2.connect(pguser.arg)
+    cur = connector.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    count = 0
+    try:
+        cur.execute("""SELECT count(*) FROM problems;""")
+        connector.commit()
+        for row in cur:
+            count = row['count']
+    except Exception as e:
+        print(e.message)
+    cur.close()
+    connector.close()
+    return count
+
+count = fetch_count_problems()
+
 def fetch_users_solveds():
     connector = psycopg2.connect(pguser.arg)
     cur = connector.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -39,7 +56,7 @@ def fetch_users_solveds():
 
 def sim_distance(users,person1,person2):
     #return len(users[person1] ^ users[person2])
-    return 1./(1.+math.sqrt(len(users[person1] ^ users[person2])))
+    return 1./(1 + len(users[person1] ^ users[person2]))
 
 def fetch_user():
     connector = psycopg2.connect(pguser.arg)
@@ -72,19 +89,21 @@ def fetch_uid(userid):
     return uid
 
 if __name__ == "__main__":
-    userid="murashin"
+    userid="ugwis"
+    uid=fetch_uid(userid)
     user_solved = fetch_users_solveds()
     users = fetch_user()
-    sim_user = []
+    cand = {}
     for user in users:
         if user['uid'] in user_solved:
-            distance = sim_distance(user_solved,fetch_uid(userid),user['uid'])
-            sim_user.append([
-                user['userid'],
-                distance
-            ])
-    sim_user = sorted(sim_user,key=lambda x:x[1], reverse=True)
-    print(sim_user)
-    """for user in sim_user[:20]:
-        print(user)"""
+            distance = sim_distance(user_solved,uid,user['uid'])
+            print(user['userid'] + " " + str(distance))
+            for solved_pid in user_solved[user['uid']] - user_solved[uid]:
+                if not solved_pid in cand:
+                    cand[solved_pid] = 0.0
+                cand[solved_pid] += distance
+    recommended_pid = max(cand,key=(lambda x:cand[x]))
+    print(str(recommended_pid) + " " + str(cand[recommended_pid]))
+    """for k,v in sorted(cand.items(),key=lambda x:x[1]):
+        print(k,v)"""
     exit(0)
